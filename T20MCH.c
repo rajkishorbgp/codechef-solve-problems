5a16d21cd295f923337b465e29add8a55e28d7a8
Raj kishor <rajkishor6091@gmail.com>
2022-05-21 22:29:37 +0530

    Problem Code: T20MCH

---
+++ T20MCH.c
@@ -0,0 +1,34 @@
+/*
+    Chef is playing in a T20 cricket match. In a match, Team A plays for 20 overs.
+    In a single over, the team gets to play 6 times, and in each of these 6 tries,
+    they can score a maximum of 6 runs. After Team A's 20 overs are finished,
+    Team B similarly plays for 20 overs and tries to get a higher total score than the first team. 
+    The team with the higher total score at the end wins the match.
+
+    Chef is in Team B. Team A has already played their 20 overs, and have gotten a score of R. 
+    Chef's Team B has started playing, and have already scored C runs in the first O overs.
+    In the remaining 20−O overs, find whether it is possible for Chef's Team B to get a score high enough to win the game.
+    That is, can their final score be strictly larger than R?        
+                                                                            */
+
+#include<stdio.h>
+void main()
+{
+	int R,O,C;
+	int sO,sR,B;
+	
+	scanf("%d%d%d",&R,&O,&C);
+   
+    sO=20-O;      //  In the remaining 20−O overs
+    sR=sO*6*6;    //  overs
+	B=sR+C;      //   total score that Team B
+	
+	if (R<B)
+	{
+		printf("YES");
+	}
+	else
+	{
+		printf("NO");
+	}
+}
